a75378d40de02b63e26b8df19c09d38f65c5904d
fodorbalazs <fodorbalazs98@gmail.com>
2021-01-05 20:52:46 +0100

    beta_verzio_gyakorlas_read_file_1.0

--- app/src/main/java/hu/nye/kondibetyar/gyakorlatok/GyakorlatokActivity.java
+++ app/src/main/java/hu/nye/kondibetyar/gyakorlatok/GyakorlatokActivity.java
@@ -5,18 +5,32 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import hu.nye.kondibetyar.R;
 
 public class GyakorlatokActivity extends AppCompatActivity {
-
-    private TextView title;
-
+    public TextView tv_text;
+    public  String text="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gyakorlatok);
 
-        title=this.findViewById(R.id.gyakorlatok_fotitle);
-        title.setText("Válassz izomcsoportot!");
+        tv_text=findViewById(R.id.tv_text);
+        try {
+            InputStream is=getAssets().open("test");
+            int size=is.available();
+            byte[] buffer=new byte[size];
+            is.read(buffer);
+            is.close();
+            text=new String(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        tv_text.setText(text);
+
+
     }
 }
