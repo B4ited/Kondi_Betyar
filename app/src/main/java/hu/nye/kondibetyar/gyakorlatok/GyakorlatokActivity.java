5c1ad3465ae4e79e2c5957234b019d6201a3e71d
Tauter <harangi.ftam@gmail.com>
2021-01-06 14:19:52 +0100

    Gyakorlatok kezdet 0.1.6 biztonsági mentés menu timer gyakorlatok kategoria activity

--- app/src/main/java/hu/nye/kondibetyar/gyakorlatok/GyakorlatokActivity.java
+++ app/src/main/java/hu/nye/kondibetyar/gyakorlatok/GyakorlatokActivity.java
@@ -1,6 +1,9 @@
 package hu.nye.kondibetyar.gyakorlatok;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +13,20 @@ import hu.nye.kondibetyar.R;
 public class GyakorlatokActivity extends AppCompatActivity {
 
     private TextView title;
-
+    private Button mell;
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gyakorlatok);
-
+        mell = this.findViewById(R.id.gyakorlatokKategoriaMell);
+        intent = new Intent(this, GyakorlatokKategoriaActivity.class);
+        mell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
         title=this.findViewById(R.id.gyakorlatok_fotitle);
         title.setText("Válassz izomcsoportot!");
     }
