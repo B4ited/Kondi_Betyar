3825bee829dc645ddda0ade4fccc1edf63b4e175
Tauter <harangi.ftam@gmail.com>
2021-01-10 12:55:13 +0100

    Gyakorlatok kezdet 0.2 biztonsági mentés mert nem bírom feldolgozni hogy MEGINT TÖRÖLTE Izom csoporton belüli gyakorlatok beolvasása fájlból

--- app/src/main/java/hu/nye/kondibetyar/gyakorlatok/GyakorlatokKategoriaActivity.java
+++ app/src/main/java/hu/nye/kondibetyar/gyakorlatok/GyakorlatokKategoriaActivity.java
@@ -1,30 +1,84 @@
 package hu.nye.kondibetyar.gyakorlatok;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+
 import hu.nye.kondibetyar.R;
 
 public class GyakorlatokKategoriaActivity extends AppCompatActivity {
-
     private TextView title;
+    private TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gyakorlatok);
+        setContentView(R.layout.activity_gyakorlatok_kategoria);
 
         title=this.findViewById(R.id.gyakorlatok_fotitle);
         Intent intent = getIntent();
         String kategoria = intent.getStringExtra("kategoria");
         title.setText(kategoria);
 
-        if(kategoria.equals("Mellizom Gyakorlatok")){
-            title.setText("I'm in");
+        test=this.findViewById(R.id.gyakorlatok_test);
+        RandomAccessFile raf;
+        
+        int n = 0;
+        String sor = null;
+        String[] kategoriak = null;
+        AssetManager am = this.getAssets();
+        BufferedReader br;
+        try {
+            InputStream is = am.open("gyakorlatCim.txt");
+            br = new BufferedReader(new InputStreamReader(is));
+
+            while ((sor = br.readLine()) !=null){
+                n++;
+            }
+            //.reset() nem működik ???
+            is = am.open("gyakorlatCim.txt");
+            br = new BufferedReader(new InputStreamReader(is));
+
+            kategoriak = new String[n];
+            n = 0;
+            while ((sor = br.readLine()) !=null){
+                kategoriak[n] = sor;
+                n++;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int kezdetIndex = 0, vegIndex = 0;
+
+        switch (kategoria){
+            case "Mellizom Gyakorlatok":
+                for (int i = 0; i < n;i++){
+                    if(kategoriak[i].equals("mell"))
+                        kezdetIndex = i;
+                    if(kategoriak[i].equals("mellVeg"))
+                        vegIndex = i;
+                }
+                break;
+            case "Hátizom Gyakorlatok":
+                for (int i = 0; i < n;i++){
+                    if(kategoriak[i].equals("hat"))
+                        kezdetIndex = i;
+                    if(kategoriak[i].equals("hatVeg"))
+                        vegIndex = i;
+                }
+                break;
         }
 
+        //generálás
     }
 }
