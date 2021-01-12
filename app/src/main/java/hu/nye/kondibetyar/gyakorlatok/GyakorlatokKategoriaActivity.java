4006f1f4d6a7eee9da54d62cb25da26206f7b87c
Tauter <harangi.ftam@gmail.com>
2021-01-12 16:52:46 +0100

    Gyakorlatok kezdet 0.4 Biztonsági mentés Gyakorlat beolvasva

--- app/src/main/java/hu/nye/kondibetyar/gyakorlatok/GyakorlatokKategoriaActivity.java
+++ app/src/main/java/hu/nye/kondibetyar/gyakorlatok/GyakorlatokKategoriaActivity.java
@@ -2,7 +2,9 @@ package hu.nye.kondibetyar.gyakorlatok;
 
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -22,23 +24,61 @@ import hu.nye.kondibetyar.R;
 public class GyakorlatokKategoriaActivity extends AppCompatActivity {
     private TextView title;
     private TextView test;
+    private String TAG = "Konzol kiírás";
 
     protected void kategoriaGeneralas(String kategoria, String[] kategoriak, int kezdet, int veg){
         LinearLayout linearLayout = new LinearLayout(this);
         setContentView(linearLayout);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
+        TextView title = new TextView(this);
+        title.setText(kategoria);
+        linearLayout.addView(title);
         for(int i = kezdet; i <= veg; i++){
             TextView tv = new TextView(this);
             tv.setText(kategoriak[i]);
             linearLayout.addView(tv);
-            tv.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    
-                }
-            });
         }
     }
+    protected void gyakorlatKiiras(String gyakorlat){
+        int n = 0,kezdetIndex = 0, vegIndex = 0,temp= 0;
+        String sor = null;
+        String gyakorlatLeiras = null;
+        AssetManager am = this.getAssets();
+        BufferedReader br;
+        Log.e(TAG,"Mi a jó élet a bajod ? :'( 😭😭😭😭");
+        try {
+            InputStream is = am.open("gyakorlatok.txt");
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((sor = br.readLine()) !=null){
+                if(sor.equals(gyakorlat))
+                    kezdetIndex = n;
+                else if (sor.equals(gyakorlat+"Veg"))
+                    vegIndex = n;
+                n++;
+            }
+
+            is = am.open("gyakorlatok.txt");
+            br = new BufferedReader(new InputStreamReader(is));
+            n = 0;
+            while ((sor = br.readLine()) !=null){
+               if(n> kezdetIndex+1 && n < vegIndex){
+                   gyakorlatLeiras += sor+"\r\n";
+                   temp++;
+               }
+               n++;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //bug fix
+       gyakorlatLeiras =  gyakorlatLeiras.replace("null","a");
+
+        //kiírás
+        test.setText(gyakorlatLeiras);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +91,7 @@ public class GyakorlatokKategoriaActivity extends AppCompatActivity {
         title.setText(kategoria);
 
         test=this.findViewById(R.id.gyakorlatok_test);
-        
+
         int n = 0;
         String sor = null;
         String[] kategoriak = null;
@@ -89,6 +129,8 @@ public class GyakorlatokKategoriaActivity extends AppCompatActivity {
                     if(kategoriak[i].equals("mellVeg"))
                         vegIndex = i-1;
                 }
+                // kategoriaGeneralas("Mellizom",kategoriak,kezdetIndex,vegIndex);
+                    gyakorlatKiiras(kategoriak[kezdetIndex+1]);
                 break;
             case "hát":
                 title.setText("Hátizom gyakorlatok");
@@ -98,8 +140,9 @@ public class GyakorlatokKategoriaActivity extends AppCompatActivity {
                     if(kategoriak[i].equals("hatVeg"))
                         vegIndex = i-1;
                 }
+                kategoriaGeneralas("Hátizom",kategoriak,kezdetIndex,vegIndex);
                 break;
-            case "váll":
+            case "vall":
                 title.setText("Vállizom gyakorlatok");
                 for (int i = 0; i < n;i++){
                     if(kategoriak[i].equals("vall"))
@@ -107,6 +150,7 @@ public class GyakorlatokKategoriaActivity extends AppCompatActivity {
                     if(kategoriak[i].equals("vallVeg"))
                         vegIndex = i-1;
                 }
+                kategoriaGeneralas("Vállizom",kategoriak,kezdetIndex,vegIndex);
                 break;
         }
 
