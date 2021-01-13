14c2adf1e6afa96b6d4ac449b2f90075645e6167
fodorbalazs <fodorbalazs98@gmail.com>
2021-01-13 10:06:53 +0100

    beta_verzio_1.1 icon_hozzáadva

--- app/src/main/java/hu/nye/kondibetyar/kapcsolat/KapcsolatActivity.java
+++ app/src/main/java/hu/nye/kondibetyar/kapcsolat/KapcsolatActivity.java
@@ -2,15 +2,37 @@ package hu.nye.kondibetyar.kapcsolat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Path;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
 
 import hu.nye.kondibetyar.R;
+import hu.nye.kondibetyar.etrend.EtrendWebViewerActivity;
 
 public class KapcsolatActivity extends AppCompatActivity {
+    private ImageButton menu;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kapcsolat);
+        menu=findViewById(R.id.ib_menu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenActivity("Menu");
+
+            }
+        });
+
+    }
+    public void OpenActivity(String Activity) {
+        if (Activity == "Menu") intent = new Intent(this, Menu.class);
+        startActivity(intent);
+
     }
 }
